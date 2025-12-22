4e4fb3d2b63483714bd01b7a3bcb10f2be1445fc
Manoela H. <manoela.n20@gmail.com>
2025-12-22 12:51:24 -0300

    nova feature

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import './styles/index.css';
 import './styles/App.css';
 import App from "./App.tsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
